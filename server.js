98e6ca6c769fe1ba3411a628252e1e4d9f67edce
Majelvey <mckelvey.andy@gmail.com>
2022-06-21 17:00:54 -0400

    changes made to utils

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils');
+const helpers = require('./utils/auth.js');
 const sequelize = require('./config/connection');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Create the Handlebars.js engine object with custom helper functions
 const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
